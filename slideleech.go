85e932fb1433079300912b3caf9f5aa3a0cba0e1
Ashley Roach <asroach@cisco.com>
2016-10-17 13:42:15 -0600

    Update to use common slide creation method

--- slideleech.go
+++ slideleech.go
@@ -1,14 +1,15 @@
 package main
 
 import (
-	"errors"
-	"fmt"
 	"bufio"
-	"os"
+	"errors"
 	"flag"
+	"fmt"
 	"html/template"
-	"gopkg.in/yaml.v2"
 	"io/ioutil"
+	"os"
+
+	"gopkg.in/yaml.v2"
 )
 
 var outputDir string
@@ -26,22 +27,22 @@ var revealClosingColor string
 
 type SlideEntry struct {
 	Content string
-	Color string
+	Color   string
 }
 
 type Config struct {
-	InputFile string `yaml:"input_file"`
-	OutputDir string `yaml:"output_directory"`
+	InputFile  string `yaml:"input_file"`
+	OutputDir  string `yaml:"output_directory"`
 	OutputFile string `yaml:"output_filename"`
-	Reveal struct {
-		Templ string `yaml:"template"`
-		TemplColor string `yaml:"template_color"`
-		Intro string `yaml:"intro"`
-		IntroTitle string `yaml:"intro_title"`
-		IntroAuthor string `yaml:"intro_author"`
-		IntroColor string `yaml:"intro_color"`
-		Closing string `yaml:"closing"`
-		ClosingMsg string `yaml:"closing_message"`
+	Reveal     struct {
+		Templ        string `yaml:"template"`
+		TemplColor   string `yaml:"template_color"`
+		Intro        string `yaml:"intro"`
+		IntroTitle   string `yaml:"intro_title"`
+		IntroAuthor  string `yaml:"intro_author"`
+		IntroColor   string `yaml:"intro_color"`
+		Closing      string `yaml:"closing"`
+		ClosingMsg   string `yaml:"closing_message"`
 		ClosingColor string `yaml:"closing_color"`
 	}
 }
@@ -57,7 +58,7 @@ func (c *Config) Parse(data []byte) error {
 		return errors.New("Slideleech config: invalid `output_directory`")
 	}
 	if c.OutputFile == "" {
-		return errors.New("Slideleech config: invalid `output_file`")
+		return errors.New("Slideleech config: invalid `output_filename`")
 	}
 
 	inputFile = c.InputFile
@@ -89,94 +90,110 @@ func init() {
 	// flag.StringVar(&outputDir, "o", "./slides", "output directory")
 	// flag.StringVar(&templateFile, "t", "", "full path to RevealJS template")
 	flag.Usage = func() {
-	fmt.Fprintf(os.Stderr, "\n*****************************************\n"+
-		"This is the slideleech.  It will extract "+
-		"your slide text/bullets contained in a markdown file.\n\n"+
-		"Enclose your slide text/bullets in "+
-		"`[item]: # (slide)` and `[item]: # (/slide)`.\n"+
-		"Any content between those tags will be added to your slide file.\n"+
-		"Include as many opening and closing tag pairs as you like "+
-		"in your Markdown.\n\n"+
-		"Edit a `.leech.yml` to configure your project\n\n",
-		os.Args[0])
-	flag.PrintDefaults()
-  }
+		fmt.Fprintf(os.Stderr, "\n*****************************************\n"+
+			"This is the slideleech.  It will extract "+
+			"your slide text/bullets contained in a markdown file.\n\n"+
+			"Enclose your slide text/bullets in "+
+			"`[item]: # (slide)` and `[item]: # (/slide)`.\n"+
+			"Any content between those tags will be added to your slide file.\n"+
+			"Include as many opening and closing tag pairs as you like "+
+			"in your Markdown.\n\n"+
+			"Edit a `.leech.yml` to configure your project\n\n")
+		flag.PrintDefaults()
+	}
 }
 
-
 func check(e error) {
-    if e != nil {
-        panic(e)
-    }
+	if e != nil {
+		panic(e)
+	}
+}
+
+type specialSlide struct {
+	template string
+	fields   map[string]string
+}
+
+func CreateSpecialSlide(outputFileName string, slide specialSlide) {
+
+	slideFile, err := os.Create(outputDir + "/" + outputFileName)
+
+	templ, err := template.New("").Parse(slide.template)
+	err = templ.Execute(slideFile, slide.fields)
+	check(err)
+
+	slideFile.Close()
 }
 
 func CreateIntroSlide(outputFileName string) {
-	type IntroSlide struct {
-		Title string
-		Author string
-	}
-	slide := IntroSlide{revealIntroTitle, revealIntroAuthor}
-	templ, err := template.New("slide0.md").Parse(INTRO_SLIDE)
-	introFile, err := os.Create(outputDir + "/" + outputFileName + "0.md")
-	fmt.Printf("Creating intro slide...\n")
-	err = templ.Execute(introFile, slide)
-	check(err)
-	introFile.Close()
+
+	m := make(map[string]string)
+	m["Title"] = revealIntroTitle
+	m["Author"] = revealIntroAuthor
+
+	var slide specialSlide
+
+	slide.template = INTRO_SLIDE
+	slide.fields = m
+
+	slideName := outputFileName + "0.md"
+	fmt.Printf("Creating intro slide... %s\n", slideName)
+	CreateSpecialSlide(slideName, slide)
 }
 
 func CreateClosingSlide(slideCount int, outputFileName string) {
-	type ClosingSlide struct {
-		Message string
-	}
-	slideName := fmt.Sprintf(outputFileName +"%d.md", slideCount)
-	slide := ClosingSlide{revealClosingMsg}
-	templ, err := template.New("slideEnd.md").Parse(CLOSING_SLIDE)
-	introFile, err := os.Create(outputDir + "/" + slideName)
-	fmt.Printf("Creating closing slide... %s\n", slideName)
-	err = templ.Execute(introFile, slide)
-	check(err)
-	introFile.Close()
-}
 
+	m := make(map[string]string)
+	m["Message"] = revealClosingMsg
+
+	var slide specialSlide
+	slide.template = CLOSING_SLIDE
+	slide.fields = m
+
+	slideName := fmt.Sprintf(outputFileName+"%d.md", slideCount)
+
+	fmt.Printf("Creating closing slide... %s\n", slideName)
+	CreateSpecialSlide(slideName, slide)
+}
 
 func CreateSite(slideCount int, outputFileName string) {
 
-    var slides []SlideEntry
+	var slides []SlideEntry
 
-		// add intro slide to slice
-		var intro SlideEntry
-		intro.Content = outputFileName + "0.md"
-		intro.Color = revealIntroColor
-		slides = append(slides, intro)
+	// add intro slide to slice
+	var intro SlideEntry
+	intro.Content = outputFileName + "0.md"
+	intro.Color = revealIntroColor
+	slides = append(slides, intro)
 
-		// Add content slides
-    for i := 1; i <= slideCount; i++ {
-      var slideName SlideEntry
-      slideName.Content = fmt.Sprintf(outputFileName + "%d.md", i)
-			slideName.Color = revealTemplColor
-      slides = append(slides, slideName)
-    }
+	// Add content slides
+	for i := 1; i <= slideCount; i++ {
+		var slideName SlideEntry
+		slideName.Content = fmt.Sprintf(outputFileName+"%d.md", i)
+		slideName.Color = revealTemplColor
+		slides = append(slides, slideName)
+	}
 
-		// add closing slide to slice
-		var closing SlideEntry
-		closing.Content = fmt.Sprintf(outputFileName + "%d.md", slideCount + 1)
-		closing.Color = revealClosingColor
-		slides = append(slides, closing)
+	// add closing slide to slice
+	var closing SlideEntry
+	closing.Content = fmt.Sprintf(outputFileName+"%d.md", slideCount+1)
+	closing.Color = revealClosingColor
+	slides = append(slides, closing)
 
-    // Include custom template based on a flag
-    templ := template.New("index.html")
-    if templateFile != "" {
-      fmt.Println("Creating RevealJS index.html from EXTERNAL template...")
-      templ, _ = templ.ParseFiles(templateFile)
-    } else {
-      fmt.Println("Creating RevealJS index.html from INTERNAL template...")
-      templ, _ = templ.Parse(INDEX_TEMPLATE)
-    }
-    // Save the template to the output directory
-    indexFile, err := os.Create(outputDir + "/" + outputFileName + ".html")
-    err = templ.Execute(indexFile, slides)
-    indexFile.Close()
-    check(err)
+	// Include custom template based on a flag
+	templ := template.New("index.html")
+	if templateFile != "" {
+		fmt.Println("Creating RevealJS index.html from EXTERNAL template...")
+		templ, _ = templ.ParseFiles(templateFile)
+	} else {
+		fmt.Println("Creating RevealJS index.html from INTERNAL template...")
+		templ, _ = templ.Parse(INDEX_TEMPLATE)
+	}
+	// Save the template to the output directory
+	indexFile, err := os.Create(outputDir + "/" + outputFileName + ".html")
+	err = templ.Execute(indexFile, slides)
+	indexFile.Close()
+	check(err)
 
 }
 
@@ -194,14 +211,14 @@ func CreateSlides(inputFile string, outputFileName string) int {
 
 	fmt.Println("Creating slides...")
 
-	for slideNum = 1; scanner.Scan();  {
+	for slideNum = 1; scanner.Scan(); {
 
 		value := scanner.Text()
 
 		if value == "[item]: # (slide)" {
 
 			matching = true
-			slideFileName := fmt.Sprintf(outputDir + "/" + outputFileName + "%d.md", slideNum)
+			slideFileName := fmt.Sprintf(outputDir+"/"+outputFileName+"%d.md", slideNum)
 			fmt.Println(slideFileName)
 
 			var err error
@@ -232,7 +249,7 @@ func CreateSlides(inputFile string, outputFileName string) int {
 }
 
 func main() {
-	flag.Parse();
+	flag.Parse()
 
 	fmt.Println("Output Directory:", outputDir)
 	fmt.Println("Input Filename:", inputFile)
@@ -245,7 +262,7 @@ func main() {
 
 	CreateIntroSlide(outputFile)
 	slideNum := CreateSlides(inputFile, outputFile)
-	CreateSite(slideNum - 1, outputFile)
+	CreateSite(slideNum-1, outputFile)
 	CreateClosingSlide(slideNum, outputFile)
 
 }
